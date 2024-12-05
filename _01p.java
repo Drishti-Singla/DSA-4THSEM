e88905c8e0abb52e4fd112199e387cc929fe7ecc
drishti <drishtisingla868@gmail.com>
2024-12-05 16:49:54 +0530

    dsa

---
+++ _01p.java
@@ -0,0 +1,12 @@
+//traversing an array
+//package BASICS;
+
+class _01p {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+        // traverse each element of array 
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("Element at index " + i + " is " + arr[i]);
+        }
+    }
+}
